7e95d02e25fabfcc5507c0db1c5b2f32582dd050
Charan12092002 <chandrasekharc702@gmail.com>
2024-07-12 16:56:39 +0530

    first commit

---
+++ frontend/src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1> Wireless Bluetooth</h1>
+      <img
+        src="img/2.webp"
+        alt="Mexicon Bluetooth"
+        width="500px"
+        height="500px"
+      />
+    </div>
+  );
+}
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My product lannding page</h1>
+      <h3>
+        <b>Wireless Connectivity:</b> Bluetooth enables devices to communicate
+        wirelessly over short distances (up to 30 feet or 10 meters),
+        eliminating the need for physical connections like cables.
+        <br />
+        <br />
+        <b>Compatibility: </b>It is a widely adopted standard supported by
+        smartphones, tablets, laptops, headphones, speakers, and IoT devices,
+        facilitating seamless connectivity between different brands and types of
+        devices. Low Power Consumption: Bluetooth is designed to be
+        energy-efficient, making it suitable for battery-powered devices.
+        Bluetooth Low Energy (BLE) further optimizes power usage, extending
+        battery life. Versatility: Supports various profiles and protocols for
+        specific applications such as audio streaming, file transfer, wireless
+        peripherals (keyboards, mice), and IoT connectivity. Ease of Use:
+        Pairing Bluetooth devices is typically straightforward, involving
+        discovery and connection processes. Devices can automatically reconnect
+        when in range, providing a user-friendly experience. Security:
+        Incorporates encryption and authentication protocols to protect data
+        transmitted between devices, ensuring privacy and preventing
+        unauthorized access. Applications: Used in audio devices (headphones,
+        speakers), peripherals (keyboards, mice), smart home devices (IoT),
+        healthcare (medical devices), automotive (hands-free calling), and more.
+        Future Trends: Continues to evolve with Bluetooth Low Energy (BLE) for
+        IoT applications and Bluetooth Mesh for large-scale networks, expanding
+        its capabilities in smart home and industrial automation.
+      </h3>
+    </div>
+  );
+}
